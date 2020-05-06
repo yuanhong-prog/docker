d38e3e146862000118c75c3af4e178db738374b5
YuanHong <gaoyf6677@gmail.com>
2020-05-06 16:31:42 +0800

    修改ignore

--- mysql/run.sh
+++ mysql/run.sh
@@ -1,5 +1,6 @@
 VERSION=5.6.28
 PORT=3306
 ROOT_PASSWORD=123456
+VOLUMES=$PWD/volumes
 
-docker run -d -p ${PORT}:3306 --name mysql-${VERSION} -e TZ=Asia/Shanghai -v $PWD/conf:/etc/mysql/conf.d -v $PWD/logs:/var/log/mysql -v $PWD/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=${ROOT_PASSWORD} mysql:${VERSION}
+docker run -d -p ${PORT}:3306 --name mysql-${VERSION} -e TZ=Asia/Shanghai -v ${VOLUMES}/conf:/etc/mysql/conf.d -v ${VOLUMES}/logs:/var/log/mysql -v ${VOLUMES}/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=${ROOT_PASSWORD} mysql:${VERSION}
