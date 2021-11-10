e5b9f1726c60f4989caebf8949d2a9d590a3f509
YuanHong <gaoyf6677@gmail.com>
2021-11-10 15:12:41 +0800

    redis优化

--- redis/run.sh
+++ redis/run.sh
@@ -1,5 +1,5 @@
 VERSION=6.0.5
-PORT=16379
+PORT=6379
 VOLUMES=$PWD/volumes
 PWD=123456
-docker run -d --name redis-${VERSION} -p ${PORT}:6379 -v ${VOLUMES}:/data redis:${VERSION} --appendonly yes --requirepass ${PWD}
+docker run --name redis-${VERSION} -p ${PORT}:6379 -v ${VOLUMES}/redis.conf:/etc/redis.conf -v ${VOLUMES}:/data -d redis:${VERSION} redis-server /etc/redis.conf --appendonly yes --requirepass ${PWD}
